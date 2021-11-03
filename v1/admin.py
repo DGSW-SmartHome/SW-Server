48f605c1796df0551256edd141ac30017a64cb25
selenium_tutorial <leatherback___@dgsw.hs.kr>
2021-11-03 19:46:23 +0900

    [CHANGE] Admin Page

--- v1/admin.py
+++ v1/admin.py
@@ -3,3 +3,6 @@ from . import models
 
 admin.site.register(models.User)
 admin.site.register(models.fineDustInfo)
+admin.site.register(models.weatherInfo)
+admin.site.register(models.userRoomLight)
+admin.site.register(models.userRoomPlug)
